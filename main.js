24501e28fde99568391e243ccbc81ea325dd8cf4
Sandesh9834 <78275234+Sandesh9834@users.noreply.github.com>
2021-09-21 00:32:00 +0530

    final commit

--- main.js
+++ main.js
@@ -2,18 +2,9 @@ var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-paid");
 var btnReturn = document.querySelector("#cash-return");
 var displayErrorMessage = document.querySelector("#error");
-// var displayErrorGif = document.querySelector("#gif");
-var img = document.getElementById('gif')
 var noOfNotes = document.querySelectorAll(".no-of-notes");
 
 var notesAvailable = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
-
-function displayErrorGif()
-{
-      var sourceOfPicture = "/images/laver-dishes.gif";
-      img.src = sourceOfPicture.replace('90x90', '225x225');
-      img.display.innerHTML = sourceOfPicture
-}
 
 function clickEventHandler()
 {
@@ -21,8 +12,11 @@ function clickEventHandler()
       {
             if(cashGiven.value < billAmount.value)
             {
-                  displayErrorMessage.innerText = "If you do not have enough money wash plates!";
-                  displayErrorGif()
+                  displayErrorMessage.innerText = "Do you want to wash plates?";
+            }
+            else if(cashGiven.value === billAmount.value)
+            {
+                  displayErrorMessage.innerText = "You are good to go";
             }
             else
             {
@@ -32,13 +26,13 @@ function clickEventHandler()
       }    
       else
       {
-            displayErrorMessage.innerText = "Enter valid Bill Amount ";
+            displayErrorMessage.innerText = "Enter valid Bill Amount";
       }
 }
 
 function returnCash(returnAmount)
 {
-     
+      
       for(var i = 0; i<notesAvailable.length; i++)
       {
             const numberOfNotes = Math.trunc(returnAmount/notesAvailable[i]);
